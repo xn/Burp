12053a5f9873bf219f8dee706cec97374f0d181e
xn <xn@rfn.noimg.com>
2010-07-22 05:09:04 -0400

    rspec, thank you

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+
+
+gem 'activesupport', '2.3.8'
+gem 'actionpack', '2.3.8'
+require 'active_support'
+require 'action_pack'
+require 'action_view'
+require 'action_controller'
+
+
+
+gem 'rspec', '>= 1.2.6'
+gem 'rspec-rails', '>= 1.2.6'
+
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/burp'))
